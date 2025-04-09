f9c9289bbd3b0fffbbdd89cf389bc7615b03da6e
Shuji-NITTC24s <s24050@tokyo.kosen-ac.jp>
2025-04-09 12:05:14 +0900

    Add files via upload

---
+++ 2025-4-9/p01ex01.c
@@ -0,0 +1,24 @@
+﻿/***   p01ex01.c ***/
+/***   ps14      ***/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Happy birthday to you.\n");
+    printf("Happy birthday to you.\n");
+    printf("Happy birthday, dear Shuji\n");
+    printf("Happy birthday to you.\n");
+
+    return 0;
+}
+
+/*** 結果 ***
+
+Happy birthday to you.
+Happy birthday to you.
+Happy birthday, dear Shuji
+Happy birthday to you.
+
+
+*************/
